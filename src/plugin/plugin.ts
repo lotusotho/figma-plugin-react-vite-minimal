c1487f6d450b0174bb8825045fb86d52047096f3
Alejandro Ramos <lotusotho11@gmail.com>
2024-03-26 12:01:56 +0100

    Delete extra and leave minimal setup

--- src/plugin/plugin.ts
+++ src/plugin/plugin.ts
@@ -1,16 +1,12 @@
 import * as Networker from "monorepo-networker";
-import { initializeNetwork } from "@common/network/init";
-import { NetworkSide } from "@common/network/sides";
-import { NetworkMessages } from "@common/network/messages";
 
 async function bootstrap() {
-  initializeNetwork(NetworkSide.PLUGIN);
 
   if (figma.editorType === "figma") {
     figma.showUI(__html__, {
       width: 800,
       height: 650,
-      title: "My Figma Plugin!",
+      title: "My new Figma Plugin!",
     });
   } else if (figma.editorType === "figjam") {
     figma.showUI(__html__, {
@@ -21,8 +17,6 @@ async function bootstrap() {
   }
 
   console.log("Bootstrapped @", Networker.Side.current.getName());
-
-  NetworkMessages.HELLO_UI.send({ text: "Hey there, UI!" });
 }
 
 bootstrap();
